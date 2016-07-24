6e1202e1c82bd62c4123c125363b5c8998be4037
Ricardo Marau <rmarau@gmail.com>
2016-07-24 21:29:16 +0100

    supportFragmentManager

--- sample/src/main/java/com/wdullaer/datetimepickerexample/DateTimePickerFragment.java
+++ sample/src/main/java/com/wdullaer/datetimepickerexample/DateTimePickerFragment.java
@@ -4,7 +4,7 @@ package com.wdullaer.datetimepickerexample;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
